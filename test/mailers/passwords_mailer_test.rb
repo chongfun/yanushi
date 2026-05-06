4b7305b205cbfeb320a084b44794ad49bddf848e
Kyle Chong <kyle@chongfun.com>
2026-05-06 16:05:32 -0700

    Add password mailer test

---
+++ test/mailers/passwords_mailer_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+
+class PasswordsMailerTest < ActionMailer::TestCase
+  test "reset" do
+    user = users(:one)
+    mail = PasswordsMailer.reset(user)
+
+    assert_equal "Reset your password", mail.subject
+    assert_equal [ user.email ], mail.to
+    assert_equal [ "from@example.com" ], mail.from # Default from ApplicationMailer
+
+    assert_match "reset your password", mail.body.encoded
+  end
+end
