a73fcfaf8b753fe02c6280e96dea9fdcfd3b258b
Kyle Chong <kyle@chongfun.com>
2026-05-24 22:14:40 -0700

    Eager load associations for dashboards
    Cleaning up n+1 queries

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,6 @@
 class DashboardsController < ApplicationController
   def index
     @properties = Current.session.user.rental_properties
+      .includes(:expenses, :tenant_payments, leases: [ :tenants, :tenant_payments, :scheduled_rents, :tenant_charges ])
   end
 end
