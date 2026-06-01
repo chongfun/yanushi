8c9e8db3df8e24a4922c673dfce787cef29979bb
Kyle Chong <kyle@chongfun.com>
2026-06-01 02:57:53 -0700

    Type dashboard controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,6 @@
 class DashboardsController < ApplicationController
   def index
-    @properties = Current.session.user.rental_properties
+    @properties = authenticated_user.rental_properties
       .includes(:expenses, :tenant_payments, leases: [ :tenants, :tenant_payments, :scheduled_rents, :tenant_charges ])
     @property_summaries = Dashboards::PropertySummariesQuery.new(properties: @properties).call
   end
